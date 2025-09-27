7526b9bee02d32a43e5241c4c229df909bf0351d
Matthew Thompson <Thompson-M62@ulster.ac.uk>
2025-09-27 15:36:49 +0100

    Implemented program code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter first number followed by the enter key");
+int firstNumber = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Enter second number followed by the enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int Result = firstNumber + secondNumber;
+
+Console.WriteLine("adding {0} and {1} equals {2}", firstNumber, secondNumber, Result);
