0311a7e26bfb8a3452a33be88fda513c769884ac
Mattias787 <Thompson-M62@ulster.ac.uk>
2025-09-29 14:45:27 +0100

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,6 +6,35 @@ int firstNumber = Convert.ToInt32 (Console.ReadLine());
 Console.WriteLine("Enter second number followed by the enter key");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-int Result = firstNumber + secondNumber;
+Console.Write("Enter the operation (+, -, *, /): ");
+
+char operation = Convert.ToChar(Console.ReadLine());
+int result = 0;
+
+ switch (operation)
+   
+        {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                Console.WriteLine("Invalid operation.");
+                return;
+        }
+Console.WriteLine($"Result: {result});
+ }
+                  
+                
+                  
+
 
 Console.WriteLine("adding {0} and {1} equals {2}", firstNumber, secondNumber, Result);
